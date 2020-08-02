73efe6301375af95bf19d61432e2fe673bb04783
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-08-03 03:10:39 +0430

    Finished

---
+++ Login-App/Login-App/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Login_App
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text != "" && txtPassword.Password != "")
+            {
+                MessageDialog msg = new MessageDialog($"Hello {txtUserName.Text} Your Password is {txtPassword.Password}", "Welcome");
+                await msg.ShowAsync();
+            }
+            else
+            {
+                MessageDialog msg = new MessageDialog("Please Enter Your Name and Password","Error");
+                await msg.ShowAsync();
+            }
+        }
+    }
+}
